031c428b8cf6a942db55a7a7afa9244236a48377
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2024-03-13 22:48:54 -0700

    add react query, fetch top 100 comments from a video

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+
+type CommentThreads = {
+  items: {
+    snippet: {
+      topLevelComment: {
+        id: string;
+        snippet: {
+          textOriginal: string;
+        };
+      };
+    };
+  }[];
+};
 
 function App() {
   const [videoId, setVideoId] = useState("");
@@ -19,7 +33,63 @@ function App() {
     getVideoId();
   }, []);
 
-  return <div>{!!videoId.length ? videoId : "Not a YouTube video."}</div>;
+  const fetchUrl = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&order=relevance&videoId=${videoId}&prettyPrint=true&key=${
+    import.meta.env.VITE_YOUTUBE_API_KEY
+  }`;
+
+  const { isFetching, isError, data, error, refetch } =
+    useQuery<CommentThreads>({
+      queryKey: ["comments"],
+      queryFn: async () => {
+        const res = await fetch(fetchUrl);
+        if (!res.ok) throw new Error("Couldn't fetch the comments!");
+        return res.json();
+      },
+      refetchOnWindowFocus: false,
+      enabled: false,
+    });
+
+  if (isError) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (isFetching) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {!!videoId.length ? (
+        !data ? (
+          <button type="button" onClick={() => refetch()}>
+            Generate the vibe check!
+          </button>
+        ) : (
+          <ul>
+            {data.items.map(
+              ({
+                snippet: {
+                  topLevelComment: { id, snippet },
+                },
+              }) => (
+                <li key={id}>{snippet.textOriginal}</li>
+              )
+            )}
+          </ul>
+        )
+      ) : (
+        "Not a YouTube video."
+      )}
+    </div>
+  );
 }
 
 export default App;
