f8def999cf7e1ab8b3383f58a21840d14199e529
mikeploythai <110638329+mikeploythai@users.noreply.github.com>
2024-03-13 21:13:54 -0700

    created manifest and get video id from current youtube url

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [videoId, setVideoId] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const getVideoId = async () => {
+      const [tab] = await chrome.tabs.query({ active: true });
+
+      if (tab.url && tab.url.includes("youtube.com/watch")) {
+        const url = new URL(tab.url);
+        const params = new URLSearchParams(url.search);
+        const id = params.get("v")!;
+        setVideoId(id);
+      }
+    };
+
+    getVideoId();
+  }, []);
+
+  return <div>{!!videoId.length ? videoId : "Not a YouTube video."}</div>;
 }
 
-export default App
+export default App;
